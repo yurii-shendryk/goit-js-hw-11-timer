2325154c600725d111a3af334d3432fe92c10c2c
yurii-shendryk <yuriishendryk93@gmail.com>
2021-02-06 15:38:19 +0200

    оновив клас CountdownTimer
    змінив конструктор, додав функцію вибору елементів таймера, в конструктор додав функцію запуску таймера.

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,35 +1,24 @@
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
-    this.root = document.querySelector(this.selector);
     this.targetDate = targetDate;
-    this.template =
-      '<div class="field"><span class="value" data-value="days">11</span ><span class="label">Days</span></div ><div class="field"><span class="value" data-value="hours">11</span><span class="label">Hours</span></div><div class="field"><span class="value" data-value="mins">11</span><span class="label">Minutes</span></div><div class="field"><span class="value" data-value="secs">11</span><span class="label">Seconds</span></div>';
-    this.root.insertAdjacentHTML('beforeend', this.template);
-    this.refs = {
-      days: this.root.querySelector('span[data-value="days"]'),
-      hours: this.root.querySelector('span[data-value="hours"]'),
-      minutes: this.root.querySelector('span[data-value="mins"]'),
-      seconds: this.root.querySelector('span[data-value="secs"]'),
-    };
-    this.startTime = this.targetDate.getTime();
-    this.currentTime = Date.now();
-    this.deltaTime = this.startTime - this.currentTime;
     this.intervalId = null;
+    this.start();
   }
+  // Методы
   // Запускаем таймер
   start() {
-    if (this.intervalId !== null) {
-      return;
-    }
-    this.deltaTime;
-    this.updateClockface(this.deltaTime);
+    const startTime = this.targetDate.getTime();
+    const currentTime = Date.now();
+    const deltaTime = startTime - currentTime;
+    this.updateClockface(deltaTime);
     this.intervalId = setInterval(() => {
       const startTime = this.targetDate.getTime();
       const currentTime = Date.now();
       const deltaTime = startTime - currentTime;
       if (deltaTime <= 0) {
         this.stop();
+        return;
       }
       this.updateClockface(deltaTime);
     }, 1000);
@@ -37,44 +26,39 @@ class CountdownTimer {
   // удаляем таймер после окончания отсчета времени
   stop() {
     clearInterval(this.intervalId);
-    this.root.innerHTML = '';
+    this.updateClockface(0);
+  }
+
+  getTimerElements(selector) {
+    const root = document.querySelector(selector);
+    const refs = {
+      days: root.querySelector('span[data-value="days"]'),
+      hours: root.querySelector('span[data-value="hours"]'),
+      minutes: root.querySelector('span[data-value="mins"]'),
+      seconds: root.querySelector('span[data-value="secs"]'),
+    };
+    return refs;
   }
 
   updateClockface(time) {
+    const timer = this.getTimerElements(this.selector);
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = this.pad(
       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
     );
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-    this.refs.days.textContent = `${days}`;
-    this.refs.hours.textContent = `${hours}`;
-    this.refs.minutes.textContent = `${mins}`;
-    this.refs.seconds.textContent = `${secs}`;
+    timer.days.textContent = `${days}`;
+    timer.hours.textContent = `${hours}`;
+    timer.minutes.textContent = `${mins}`;
+    timer.seconds.textContent = `${secs}`;
   }
   pad(value) {
     return String(value).padStart(2, '0');
   }
 }
 
-const firstTimer = new CountdownTimer({
+const timer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jun 05, 2021'),
 });
-
-firstTimer.start();
-
-setTimeout(() => {
-  firstTimer.stop();
-}, firstTimer.deltaTime);
-
-const secondTimer = new CountdownTimer({
-  selector: '#timer-2',
-  targetDate: new Date('Jul 05, 2021'),
-});
-
-secondTimer.start();
-
-setTimeout(() => {
-  secondTimer.stop();
-}, secondTimer.deltaTime);
